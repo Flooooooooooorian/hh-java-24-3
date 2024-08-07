5711263b7017bda2fbbf26588f6178ce625344a6
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2024-08-07 14:47:56 +0200

    Add React

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import Header from "./Header.tsx";
+import {Blog} from "./models.ts";
+
+export default function App() {
+
+    const name: string = "Florian"
+
+    const myBlogEntry: Blog = {
+        id: "1",
+        title: "Mein Mittwoch",
+        text: "bla"
+    }
+
+    const add = (num1: number, num2: number) => {
+        return num1 + num2
+    }
+
+    return (
+        <div>
+            <Header />
+            <p>{name + " !"} Test</p>
+            <p>{add(8, 9)}</p>
+            <button>Button</button>
+            {myBlogEntry.title}
+            {myBlogEntry.text}
+            <Header />
+        </div>
+    )
+}
---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+export default function Header() {
+
+
+    return (
+        <h1>
+            Title
+        </h1>
+    )
+}
