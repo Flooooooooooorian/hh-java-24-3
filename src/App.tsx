86ea60dcda3c130c780f7d910836473f62bd82fa
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2024-08-09 10:34:14 +0200

    Add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Header from "./Header.tsx";
 import {Blog} from "./models.ts";
+import {ChangeEvent, useState} from "react";
 
 export default function App() {
+    console.log("Render")
 
-    const name: string = "Florian"
+    const [count, setCount] = useState<number>(0)
+    const [name, setName] = useState<string>("")
+    const [visible, setVisible] = useState<boolean>(true)
 
     const myBlogEntry: Blog = {
         id: "1",
@@ -15,15 +19,41 @@ export default function App() {
         return num1 + num2
     }
 
+    const onNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log("OnChange")
+        console.log(event.target.value)
+        setName(event.target.value)
+    }
+
     return (
         <div>
-            <Header />
+            <Header/>
             <p>{name + " !"} Test</p>
             <p>{add(8, 9)}</p>
-            <button>Button</button>
+
+            <p>{count}</p>
+            <button onClick={() => {
+                setCount(count + 1)
+            }}>
+                Button
+            </button>
+
+            <input onChange={onNameChange}
+                   value={name}
+                   placeholder={"Name"}/>
+
             <p>{myBlogEntry.title}</p>
             <p>{myBlogEntry.text}</p>
-            <Header />
+            {visible && <Header/>}
+
+            {count > 10 && <p>Count ist über 10!</p>}
+            {count === 15
+                ? <p>
+                    Count is gleich 15!
+                </p>
+                : <p>
+                    Count ist nicht 15!
+                </p>}
         </div>
     )
 }
